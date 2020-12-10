52c06e59be37ec89ec61d7776f8c3a4d815e6ae5
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-12-10 13:25:21 +0900

    バリデーション設定

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,3 +1,7 @@
 class Cost < ApplicationRecord
-  
+with_options presence: true do  
+  validates :day, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
+ end
 end
